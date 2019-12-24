6b47236a23bbc25ae394908ae0787904595afe8d
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-12-24 14:54:54 +0000

    Added player animation

--- sprites.py
+++ sprites.py
@@ -12,9 +12,20 @@ class Player(pygame.sprite.Sprite):
 
         # Get the game instance (for collision use)
         self.game = game
-        
+
+        # Load in the sprites images
+        self.loadImages()
+
+        # Define variables to determine the action the player is currently taking
+        self.idle = True
+        self.walking = False
+        self.jumping = False
+        self.direction = "R"
+        self.currentImage = 0
+        self.tickCount = 0
+
         # Define the image size, color, etc
-        self.image = self.game.player_spritesheet.getImageAt(768, 768, 128, 256)
+        self.image = self.idle_frames[0]
  
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
@@ -25,12 +36,10 @@ class Player(pygame.sprite.Sprite):
         self.velocity = vec(0, 0)
         self.acceleration = vec(0, 0)
 
-        # Define variables to determine the action the player is currently taking
-        self.idle = True
-        self.walking = False
-        self.jumping = False
-
     def update(self):
+        # Update current image of sprite
+        self.animate()
+
         # Default acceleration is 0 (x-direction) and GRAVITY (y-direction)
         self.acceleration = vec(0, PLAYER_GRAVITY)  # i.e. always accelerting downwards unless changed later
         
@@ -46,6 +55,10 @@ class Player(pygame.sprite.Sprite):
         # Decrease acceleration according friction, update velocity according to acceleration
         self.acceleration.x += self.velocity.x * PLAYER_FRICTION  # Friction only applies to x-range
         self.velocity += self.acceleration
+
+        # Reset velocity when it gets too small
+        if(abs(self.velocity.x) < 0.5):
+            self.velocity.x = 0
 
         # Calculate the new position (could remove acceleration part)
         self.position += self.velocity + 0.5 * self.acceleration
@@ -71,6 +84,9 @@ class Player(pygame.sprite.Sprite):
         # Updates player position (keep track of bottom, since this is used in collisons)
         self.rect.midbottom = self.position
 
+        # Update the tick count 
+        self.tickCount += 1
+
     def jump(self):
         self.rect.y += 1
         collision = pygame.sprite.spritecollide(self, self.game.platform_sprites, False)
@@ -79,7 +95,78 @@ class Player(pygame.sprite.Sprite):
         if(collision and self.velocity.y == 0):
             self.velocity.y = -13
 
+    def animate(self):
+        self.updateStates()
+        if(self.idle):
+            print("IDLE")
+            if(self.tickCount > 30):
+                self.tickCount = 0
+                self.currentImage = (self.currentImage + 1) % len(self.idle_frames)
+                self.image = self.idle_frames[self.currentImage]
+                currentX, currentY = self.rect.midbottom
+                self.rect = self.image.get_rect()
+                self.rect.midbottom = (currentX, currentY)
+        elif(self.walking):
+            print("WALKING")
+            if(self.tickCount > 15):
+                self.tickCount = 0
+                self.currentImage = (self.currentImage + 1) % len(self.walking_frames_r)
+                if(self.velocity.x > 0): self.image = self.walking_frames_r[self.currentImage]
+                else: self.image = self.walking_frames_l[self.currentImage]
+                currentX, currentY = self.rect.midbottom
+                self.rect = self.image.get_rect()
+                self.rect.midbottom = (currentX, currentY)
+        elif(self.jumping):
+            print("JUMPING")
+            if(self.tickCount > 15):
+                self.tickCount = 0
+                self.currentImage = (self.currentImage + 1) % len(self.jumping_frames)
+                self.image = self.jumping_frames[self.currentImage]
+                currentX, currentY = self.rect.midbottom
+                self.rect = self.image.get_rect()
+                self.rect.midbottom = (currentX, currentY)
 
+    def updateStates(self):
+        # Update idle state
+        if(self.velocity.x == 0 and self.velocity.y == 0):
+            self.idle = True
+        else:
+            self.idle = False
+
+        # Update walking state (only if not jumping)
+        if(not self.jumping):
+            if(self.velocity.x != 0):
+                self.walking = True
+            else:
+                self.walking = False
+
+        # Update jumping state
+        if(self.velocity.y != 0):
+            self.jumping = True
+        elif(self.velocity.y == 0.5 and len(pygame.sprite.spritecollide(self, self.game.platform_sprites, False)) == 0): 
+            self.jumping = True
+        else:
+            self.jumping = False
+        
+
+    def loadImages(self):
+        self.idle_frames = []
+        self.walking_frames_r = []
+        self.walking_frames_l = []
+        self.jumping_frames = []
+        
+        # Update idle frame
+        self.idle_frames.append(self.game.player_spritesheet.getImageAt(768, 0, 128, 256))
+        self.idle_frames.append(self.game.player_spritesheet.getImageAt(768, 768, 128, 256))
+
+        # Update walking frames
+        self.walking_frames_r.append(self.game.player_spritesheet.getImageAt(640, 1280, 128, 256))
+        self.walking_frames_r.append(self.game.player_spritesheet.getImageAt(640, 1024, 128, 256))
+        self.walking_frames_l.append(pygame.transform.flip(self.game.player_spritesheet.getImageAt(640, 1280, 128, 256), True, False))
+        self.walking_frames_l.append(pygame.transform.flip(self.game.player_spritesheet.getImageAt(640, 1024, 128, 256), True, False))
+
+        # Update jumping frames
+        self.jumping_frames.append(self.game.player_spritesheet.getImageAt(768, 256, 128, 256))
     
 ''' PLATFORM SPRITE '''
 class Platform(pygame.sprite.Sprite):
